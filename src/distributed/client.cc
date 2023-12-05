bcc2cc9dcca13020582e47fcad2e0d745c835428
greenEggLy <green_egg@sjtu.edu.cn>
2023-12-05 11:32:24 +0800

    fix lab2

--- src/distributed/client.cc
+++ src/distributed/client.cc
@@ -7,16 +7,17 @@
 #include "distributed/metadata_server.h"
 
 namespace chfs {
+ChfsClient::ChfsClient()
+  : num_data_servers(0) {
+}
 
-ChfsClient::ChfsClient() : num_data_servers(0) {}
-
-auto ChfsClient::reg_server(ServerType type, const std::string &address,
+auto ChfsClient::reg_server(ServerType type, const std::string& address,
                             u16 port, bool reliable) -> ChfsNullResult {
   switch (type) {
     case ServerType::DATA_SERVER:
       num_data_servers += 1;
       data_servers_.insert({num_data_servers, std::make_shared<RpcClient>(
-                                                  address, port, reliable)});
+                                address, port, reliable)});
       break;
     case ServerType::METADATA_SERVER:
       metadata_server_ = std::make_shared<RpcClient>(address, port, reliable);
@@ -31,7 +32,7 @@ auto ChfsClient::reg_server(ServerType type, const std::string &address,
 
 // {Your code here}
 auto ChfsClient::mknode(FileType type, inode_id_t parent,
-                        const std::string &name) -> ChfsResult<inode_id_t> {
+                        const std::string& name) -> ChfsResult<inode_id_t> {
   auto res = metadata_server_->call("mknode", (u8)type, parent, name);
   if (res.is_err()) return {res.unwrap_error()};
   if (res.unwrap()->as<inode_id_t>() == 0) return {ErrorType::INVALID};
@@ -39,16 +40,16 @@ auto ChfsClient::mknode(FileType type, inode_id_t parent,
 }
 
 // {Your code here}
-auto ChfsClient::unlink(inode_id_t parent, std::string const &name)
-    -> ChfsNullResult {
+auto ChfsClient::unlink(inode_id_t parent, std::string const& name)
+  -> ChfsNullResult {
   auto res = metadata_server_->call("unlink", parent, name);
   if (res.is_err()) return res.unwrap_error();
   return KNullOk;
 }
 
 // {Your code here}
-auto ChfsClient::lookup(inode_id_t parent, const std::string &name)
-    -> ChfsResult<inode_id_t> {
+auto ChfsClient::lookup(inode_id_t parent, const std::string& name)
+  -> ChfsResult<inode_id_t> {
   auto res = metadata_server_->call("lookup", parent, name);
   if (res.is_err()) return res.unwrap_error();
   return {res.unwrap()->as<inode_id_t>()};
@@ -56,7 +57,7 @@ auto ChfsClient::lookup(inode_id_t parent, const std::string &name)
 
 // {Your code here}
 auto ChfsClient::readdir(inode_id_t id)
-    -> ChfsResult<std::vector<std::pair<std::string, inode_id_t>>> {
+  -> ChfsResult<std::vector<std::pair<std::string, inode_id_t>>> {
   auto res = metadata_server_->call("readdir", id);
   if (res.is_err()) return res.unwrap_error();
   return {res.unwrap()->as<std::vector<std::pair<std::string, inode_id_t>>>()};
@@ -64,7 +65,7 @@ auto ChfsClient::readdir(inode_id_t id)
 
 // {Your code here}
 auto ChfsClient::get_type_attr(inode_id_t id)
-    -> ChfsResult<std::pair<InodeType, FileAttr>> {
+  -> ChfsResult<std::pair<InodeType, FileAttr>> {
   auto res = metadata_server_->call("get_type_attr", id);
   if (res.is_err()) return res.unwrap_error();
   std::pair<InodeType, FileAttr> ret;
@@ -90,7 +91,7 @@ auto ChfsClient::get_type_attr(inode_id_t id)
  */
 // {Your code here}
 auto ChfsClient::read_file(inode_id_t id, usize offset, usize size)
-    -> ChfsResult<std::vector<u8>> {
+  -> ChfsResult<std::vector<u8>> {
   auto res = metadata_server_->call("get_block_map", id);
   if (res.is_err()) return res.unwrap_error();
   auto block_info = res.unwrap()->as<std::vector<BlockInfo>>();
@@ -114,16 +115,16 @@ auto ChfsClient::read_file(inode_id_t id, usize offset, usize size)
     block_info_index++;
     block_info_offset = 0;
   }
-  for (int i = 0; i < 30; i++) {
-    std::cerr << content[i];
-  }
-  std::cerr << std::endl;
+  // for (int i = 0; i < 30; i++) {
+  //   std::cerr << content[i];
+  // }
+  // std::cerr << std::endl;
   return {content};
 }
 
 ChfsNullResult get_or_alloc_block_info(
-    const std::shared_ptr<RpcClient> &metadata_server, inode_id_t id,
-    std::vector<BlockInfo> &block_infos, size_t index) {
+    const std::shared_ptr<RpcClient>& metadata_server, inode_id_t id,
+    std::vector<BlockInfo>& block_infos, size_t index) {
   while (block_infos.size() <= index) {
     auto res = metadata_server->call("alloc_block", id);
     if (res.is_err()) return res.unwrap_error();
@@ -134,21 +135,22 @@ ChfsNullResult get_or_alloc_block_info(
 
 // {Your code here}
 auto ChfsClient::write_file(inode_id_t id, usize offset, std::vector<u8> data)
-    -> ChfsNullResult {
+  -> ChfsNullResult {
   auto res = metadata_server_->call("get_block_map", id);
   if (res.is_err()) return res.unwrap_error();
   auto block_info = res.unwrap()->as<std::vector<BlockInfo>>();
   auto block_info_index = offset / DiskBlockSize;
   auto block_info_offset = offset % DiskBlockSize;
-  auto write_len = data.size();  // left size to write
+  auto write_len = data.size(); // left size to write
   size_t write_start = 0,
-         write_end = write_len;  // pointer to the data of start and end
+         write_end = write_len; // pointer to the data of start and end
   // if we should alloc new blocks
-  while (write_len > 0) {  // still needs to write
+  while (write_len > 0) {
+    // still needs to write
     auto left_size_in_block = DiskBlockSize - block_info_offset;
     write_end = left_size_in_block > write_len
-                    ? write_start + write_len
-                    : write_start + left_size_in_block;
+                  ? write_start + write_len
+                  : write_start + left_size_in_block;
     std::vector<u8> tmp_data(write_end - write_start);
     std::move(data.begin() + (long)write_start, data.begin() + (long)write_end,
               tmp_data.begin());
@@ -172,5 +174,4 @@ auto ChfsClient::free_file_block(inode_id_t id, block_id_t block_id,
   auto res = metadata_server_->call("free_block", id, block_id, mac_id);
   return KNullOk;
 }
-
-}  // namespace chfs
+} // namespace chfs
